32f26452aba8c2c70145845f950ee33dea408f1f
sawnaytharpoe02 <sawnaytharpoe14@gmail.com>
2023-12-12 22:47:51 +0630

    thinking layout design and sketch

--- app/components/LeftPanel.tsx
+++ app/components/LeftPanel.tsx
@@ -4,15 +4,15 @@ import React from 'react';
 import ThemeSwitcher from './ThemeSwitcher';
 import Image from 'next/image';
 import { siteConfig } from '@/config/siteConfig';
-import { Mail } from 'lucide-react';
-import Link from 'next/link';
-import { useState } from 'react';
 import { RoughNotationGroup, RoughNotation } from 'react-rough-notation';
+import { useTheme } from 'next-themes';
 
 const LeftPanel = () => {
-  const [isHovered, setIsHovered] = useState(false);
+  const { theme } = useTheme();
+
+  console.log(theme);
   return (
-    <div className="basis-1/3">
+    <div className="basis-1/3 py-10">
       {/* profile avatar */}
       <Image
         src="/profile.jpg"
@@ -29,26 +29,31 @@ const LeftPanel = () => {
       <div className="mt-6 space-y-2">
         <p className="text-4xl font-bold">{siteConfig.author}</p>
         <RoughNotationGroup show={true}>
-          <p className="text-[1.05rem] leading-relaxed tracking-wider text-neutral-700 dark:text-neutral-400">
+          <p className="text-[1.05rem] leading-relaxed tracking-wider text-neutral-800 dark:text-neutral-300">
             A meticulous{' '}
-            <RoughNotation type="highlight" color="#c097dd" strokeWidth={1}>
-              <span className="dark:text-tlight">junior web developer</span>
+            <RoughNotation
+              type="highlight"
+              color={theme === 'dark' ? '#6e22a5' : '#c097dd'}
+              strokeWidth={1}>
+              <span>junior web developer</span>
             </RoughNotation>{' '}
             with a robust love for coffee{' '}
             <img className="inline" src="/Coffee.png" width={30} />, delving
             into the{' '}
-            <RoughNotation type="highlight" color="#6edacf" strokeWidth={1}>
-              <span className="dark:text-tlight">backend's</span>
+            <RoughNotation
+              type="highlight"
+              color={theme === 'dark' ? '#069082' : '#6edacf'}
+              strokeWidth={1}>
+              <span>backend's</span>
             </RoughNotation>{' '}
             enchanting realms .
             <img className="inline mr-1" src="/Mail.png" width={25} />
             Open to new connections and collaborations—
-            <RoughNotation type="highlight" strokeWidth={1} color="#c5efc3">
-              <a
-                href={`mailto:${siteConfig.mail}`}
-                className="dark:text-tlight">
-                let's connect
-              </a>
+            <RoughNotation
+              type="underline"
+              color={theme === 'dark' ? '#E8E9F3' : '#1C1C27'}
+              strokeWidth={1}>
+              <a href={`mailto:${siteConfig.mail}`}>let's connect !!</a>
             </RoughNotation>
           </p>
         </RoughNotationGroup>
@@ -56,7 +61,7 @@ const LeftPanel = () => {
 
       {/* Footer */}
       <div className="flex justify-between items-center border-t border-neutral-200 dark:border-neutral-900 mt-6 pt-4">
-        <p className="text-neutral-700 tracking-wider dark:text-neutral-400">
+        <p className="text-neutral-800 tracking-wider dark:text-neutral-400">
           Designed and coded with love's fiery touch. ❤️‍🔥
         </p>
         <ThemeSwitcher />
--- app/components/RightPanel.tsx
+++ app/components/RightPanel.tsx
@@ -1,12 +1,30 @@
 'use client';
-import React from 'react'
+import React from 'react';
+
+// const arrayItem = Array.from({ length: 10 }, (_, i) => i + 1);
+const arrayItem = [
+  { className: 'bg-red-400 rounded-3xl col-span-2 row-span-2' },
+  { className: 'bg-red-400 rounded-3xl col-span-2 row-span-4' },
+  { className: 'bg-red-400 rounded-3xl col-span-2 row-span-4' },
+  { className: 'bg-red-400 rounded-3xl col-span-1 row-span-2' },
+  { className: 'bg-red-400 rounded-3xl col-span-1 row-span-2' },
+  { className: 'bg-red-400 rounded-3xl col-span-2 row-span-4' },
+  { className: 'bg-red-400 rounded-3xl col-span-1 row-span-2' },
+  { className: 'bg-red-400 rounded-3xl col-span-1 row-span-2' },
+  { className: 'bg-red-400 rounded-3xl col-span-2 row-span-2' },
+];
 
 const RightPanel = () => {
   return (
-    <div className="basis-2/3">
-      <p className="text-4xl">right side</p>
+    <div className="basis-2/3 overflow-y-auto min-h-screen">
+      <p className="text-3xl my-4">right side</p>
+      <div className="grid grid-cols-4 auto-rows-[78px] gap-8 mb-96">
+        {arrayItem.map((item, index) => (
+          <div className={item.className}>some fucking cool content</div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default RightPanel
+export default RightPanel;
